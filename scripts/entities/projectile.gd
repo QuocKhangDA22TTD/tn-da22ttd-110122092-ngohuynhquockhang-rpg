a655bcf2c029294b6a0fa1ae65c641e0cdcd1e75
NguyenDinhTuanKhoa <nguyendinhtuankhoatt209@gmail.com>
2026-05-22 15:23:04 +0700

    Thêm vào xử lý xóa projectile khi quá thời gian cho phép.

--- scripts/entities/projectile.gd
+++ scripts/entities/projectile.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+@export var exist_timer = 0.0
+@export var max_exist_time = 3.0
+
 var speed: float = 0.0
 var direction: Vector2 = Vector2.ZERO
 
@@ -10,9 +13,14 @@ func _physics_process(delta: float) -> void:
 
 	if rotation != direction.angle():
 		rotation = direction.angle()  # Xoay projectile theo hướng di chuyển
+	
+	exist_timer += delta
+
+	if exist_timer >= max_exist_time:
+		queue_free()  # Xóa projectile nếu đã tồn tại quá lâu
 
 # Xử lý va chạm với kẻ địch
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("enemy") and body.has_method("take_damage"):
+	if body.is_in_group("enemy") and body.has_method("take_damage") and speed != 0.0:
 		body.take_damage(2.0, self) # Gọi hàm take_damage trên kẻ địch, truyền vào lượng sát thương
 		queue_free()  # Xóa projectile sau khi va chạm
