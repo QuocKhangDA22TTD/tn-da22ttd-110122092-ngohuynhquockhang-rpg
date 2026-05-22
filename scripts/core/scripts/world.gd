61c344836dcc858af3beacd00d4af91081ea5f4f
NguyenDinhTuanKhoa <nguyendinhtuankhoatt209@gmail.com>
2026-05-22 16:34:25 +0700

    Cập nhập script world: - Thêm vào xử lý lưu tham chiếu đến node enemies để các spawner dễ dàng thêm quái vào

--- scripts/core/scripts/world.gd
+++ scripts/core/scripts/world.gd
@@ -1,11 +1,6 @@
 extends Node2D
 
+@export var enemies_container: Node2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	GameManager.enemies_container = enemies_container # Lưu tham chiếu đến container quái để các spawner dễ dàng thêm quái vào
