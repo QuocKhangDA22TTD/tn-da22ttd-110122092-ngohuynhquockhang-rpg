286c9f26d3b9a3aad895a2d8ffb6ca546d4badea
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-05-30 17:35:23 +0700

    Thêm vào nhiều biến export trong script enemy_data.

--- scripts/data/enemy_data.gd
+++ scripts/data/enemy_data.gd
@@ -10,6 +10,12 @@ class_name EnemyData
 @export var speed: float = 50
 @export var damage: int = 1
 
+# thông số hành vi
+@export var patrol_radius: float = 150.0 # bán kính mà kẻ địch sẽ di chuyển xung quanh khi ở trạng thái tuần tra
+@export var max_patrol_duration: float = 3.0 # Thời gian tối đa cho mỗi lượt đi tuần tra
+@export var chase_distance: float = 100.0 # Khoảng cách mà kẻ địch sẽ bắt đầu đuổi theo người chơi
+@export var attack_distance: float = 20.0 # Khoảng cách mà kẻ địch sẽ bắt đầu tấn công người chơi
+
 # thông tin hoạt ảnh
 @export var animation_library: AnimationLibrary
 # các trạng thái có thể có
