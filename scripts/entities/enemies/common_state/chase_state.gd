f5285c99493c9324836c6119210139e2e04f9010
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-05-31 14:59:45 +0700

    Cập nhập dùng NavigationAgent2D để điều hướng di chuyển trong script chase_state.

--- scripts/entities/enemies/common_state/chase_state.gd
+++ scripts/entities/enemies/common_state/chase_state.gd
@@ -1,24 +1,39 @@
 extends EnemyState
 class_name ChaseState
 
+var chase_distance: float # Khoảng cách mà kẻ địch sẽ bắt đầu đuổi theo người chơi
+var reaction_delay: float # Thời gian cập nhật lại mục tiêu sau khi phát hiện người chơi
+var update_timer: float = 0.0 # Biến đếm thời gian để kiểm soát tần suất cập nhật mục tiêu
+
 func enter(enemy):
-	enemy.play_anim("chase")
+	# Lấy thông số từ EnemyData để sử dụng trong quá trình tuần tra
+	chase_distance = enemy.data.chase_distance
+	reaction_delay = enemy.data.reaction_delay
+
+	enemy.play_anim("chase") # Phát hoạt ảnh đuổi theo
+
+	update_timer = 0.0 # reset timer khi vào trạng thái Chase
 
 func update(enemy, delta):
-	var distance_to_player = enemy.distance_to_player()
+	var distance_to_player = enemy.distance_to_player() # tính khoảng cách đến người chơi
 
-	if distance_to_player != null and distance_to_player > 20:
-		var dir = enemy.get_player().global_position - enemy.global_position
-		enemy.velocity = dir.normalized() * enemy.speed
-	else:
-		enemy.velocity = Vector2.ZERO
+	# Nếu người chơi nằm trong khoảng cách đuổi theo và timer đã hết, cập nhật mục tiêu đến vị trí người chơi
+	if distance_to_player != null and distance_to_player < chase_distance:
+		update_timer -= delta
 
-		var attack = enemy.get_state(AttackState)
-
-		if attack:
-			enemy.change_state(attack)
-
-	if distance_to_player != null and distance_to_player >= 100:
+		if update_timer <= 0:
+			enemy.navigation_agent_2d.target_position = GameManager.player.global_position
+			update_timer = reaction_delay # Reset lại bộ đếm
+	
+	# Nếu người chơi ra khỏi khoảng cách đuổi theo và kẻ địch đã hoàn thành việc di chuyển đến mục tiêu hiện tại, chuyển sang trạng thái Idle
+	elif distance_to_player != null and distance_to_player >= chase_distance and enemy.navigation_agent_2d.is_navigation_finished():
 		var idle = enemy.get_state(IdleState)
 		if idle:
 			enemy.change_state(idle)
+		return
+	
+	# Lấy vị trí tiếp theo trên đường đi đến mục tiêu
+	var next_path_pos: Vector2 = enemy.navigation_agent_2d.get_next_path_position()
+
+	# Tính toán vận tốc mong muốn để di chuyển về phía vị trí tiếp theo trên đường đi
+	enemy.desired_velocity = enemy.global_position.direction_to(next_path_pos) * enemy.speed
