a4d5ac4cfa2a7d8e6494bc02fd5e5494b1ac23c1
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-02 17:13:17 +0700

    Cập nhập enemy_data, thêm vào các biến export để lưu các thông số liên quan tới hành vi tấn công dash.

--- scripts/data/enemy_data.gd
+++ scripts/data/enemy_data.gd
@@ -7,15 +7,19 @@ class_name EnemyData
 
 # thông số chiến đấu
 @export var max_hp: int = 10
-@export var speed: float = 50
+@export var speed: float = 30
 @export var damage: int = 1
 
 # thông số hành vi
 @export var patrol_radius: float = 150.0 # bán kính mà kẻ địch sẽ di chuyển xung quanh khi ở trạng thái tuần tra
-@export var max_patrol_duration: float = 3.0 # Thời gian tối đa cho mỗi lượt đi tuần tra
-@export var chase_distance: float = 100.0 # Khoảng cách mà kẻ địch sẽ bắt đầu đuổi theo người chơi
-@export var attack_distance: float = 20.0 # Khoảng cách mà kẻ địch sẽ bắt đầu tấn công người chơi
-@export var reaction_delay: float = 1.0 # Thời gian cập nhật lại mục tiêu sau khi phát hiện người chơi
+@export var max_patrol_duration: float = 2.0 # Thời gian tối đa cho mỗi lượt đi tuần tra
+@export var chase_distance: float = 120.0 # Khoảng cách mà kẻ địch sẽ bắt đầu đuổi theo người chơi
+@export var attack_distance: float = 48.0 # Khoảng cách mà kẻ địch sẽ bắt đầu tấn công người chơi
+@export var reaction_delay: float = 0.2 # Thời gian cập nhật lại mục tiêu sau khi phát hiện người chơi
+
+@export var attack_dash_speed: float = 400.0    # Tốc độ lao đi khi tấn công
+@export var charge_duration: float = 1.0   # Thời gian đứng yên gồng đòn
+@export var attack_duration: float = 0.2    # Thời gian lao đi
 
 # thông tin hoạt ảnh
 @export var animation_library: AnimationLibrary
