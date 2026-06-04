3d0a620544df271832c0dc9eaf0e38ef5de5cb54
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-04 14:23:21 +0700

    Cập nhập script character_stats: - Cập nhập các giá trị của các biến export. - Xóa biến exportattack_power.

--- scripts/data/character_stats.gd
+++ scripts/data/character_stats.gd
@@ -2,11 +2,10 @@ extends Resource
 
 class_name CharacterStats
 
-@export var max_health: float = 100.0
-@export var current_health: float = 100.0 :
+@export var max_health: float = 10.0
+@export var current_health: float = 10.0 :
 	set(value):
 		current_health = clamp(value, 0, max_health)
 		emit_changed() # Thông báo khi máu thay đổi (để UI cập nhật)
 
-@export var speed: float = 150.0
-@export var attack_power: float = 10.0
+@export var speed: float = 80.0
