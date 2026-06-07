339b31bd883599001cb8f1df739d35e4429fc2ba
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-07 16:52:59 +0700

    Cập nhập script explosion. Thêm vào logic xử lý kiểm tra và gây damage.

--- scripts/entities/effects/explosion.gd
+++ scripts/entities/effects/explosion.gd
@@ -1,1 +1,24 @@
 extends Area2D
+
+@export var explosion_damage: float = 40.0
+@export var animation_player: AnimationPlayer
+
+var owner_entity: Node = null # Biến để lưu reference đến entity sở hữu explosion
+var hit_entities: Array = []
+
+func _ready() -> void:
+	animation_player.play("explosion")
+	await animation_player.animation_finished
+	queue_free()
+
+func _on_area_entered(area: Area2D) -> void:
+	print("area: ", area)
+	# Tránh double damage
+	if area == owner_entity or area in hit_entities:
+		return
+
+	# Tìm hurtbox và gây damage trực tiếp
+	if area is Hurtbox:
+		area.take_damage(explosion_damage, self)
+			
+		hit_entities.append(area)
