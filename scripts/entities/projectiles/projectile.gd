7a899af618b42f3edcbcc1e4c8d69f1cc2c3f0e9
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-07 16:50:48 +0700

    Cập nhập script projectile: - Thêm vào biến export explosion_scene chứa scene vụ nổ. - Dùng điều kiệm kiểm tra class Hurtbox thay cho kiểm tra group - Thêm vào hàm spawn_explosion để spawn vụ nổ sau khi projectile va chạm vào hurtbox. - Thêm điều kiện kiểm tra nếu biến export explosion_scene rỗng thì sẽ chạy hàm take_damage và không chạy hàm spawn_explosion, còn nếu có thì không chạy hàm take_damage mà sẽ chạy hàm spawn_explosion.

--- scripts/entities/projectiles/projectile.gd
+++ scripts/entities/projectiles/projectile.gd
@@ -4,37 +4,42 @@ extends Area2D
 @export var damage: float = 40.0
 @export var exist_timer = 0.0
 @export var max_exist_time = 3.0
+@export var explosion_scene: PackedScene
 
-var owner_entity: Node = null # Biến để lưu reference đến entity sở hữu viên đạn, dùng để tránh va chạm với chính mình
-var direction: Vector2 = Vector2.ZERO # Hướng di chuyển của viên đạn
+var owner_entity: Node = null
+var direction: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	if not area_entered.is_connected(_on_area_entered):
 		area_entered.connect(_on_area_entered)
 
 func _physics_process(delta: float) -> void:
-
 	if rotation != direction.angle():
-		rotation = direction.angle() # Xoay projectile theo hướng di chuyển
+		rotation = direction.angle() # Cập nhật rotation của projectile để hướng về direction
 	
-	var velocity = direction * speed # Tính toán vận tốc dựa trên hướng và tốc độ
-	position += velocity * delta # Cập nhật vị trí của viên đạn
+	var velocity = direction * speed
+	position += velocity * delta
 	
 	exist_timer += delta
-
 	if exist_timer >= max_exist_time:
-		queue_free()  # Xóa projectile nếu đã tồn tại quá lâu
-
+		queue_free() # Xóa projectile nếu tồn tại quá lâu mà không va chạm gì
 
 func _on_area_entered(area: Area2D) -> void:
 	if area == owner_entity:
-		return  # Không va chạm với chính mình
-
-	if area.is_in_group("enemy") and area.has_method("take_damage") and speed != 0.0:
-		area.take_damage(damage, self) # Gọi hàm take_damage trên kẻ địch, truyền vào lượng sát thương
-		queue_free()  # Xóa projectile sau khi va chạm
+		return # Tránh va chạm với chính mình
 	
-	elif area.is_in_group("player") and speed != 0.0:
-		if area.has_method("take_damage"):
-			area.take_damage(damage, self) # Gọi hàm take_damage trên player, truyền vào lượng sát thương
+	if area is Hurtbox:
+		if not explosion_scene:
+			area.take_damage(damage, self) # Tìm hurtbox và gây damage trực tiếp
+		else:
+			call_deferred("spawn_explosion") # Tạo explosion
+
 		queue_free() # Xóa projectile sau khi va chạm
+
+func spawn_explosion() -> void:
+	if explosion_scene:
+		var exp_inst = explosion_scene.instantiate()
+		exp_inst.explosion_damage = damage
+		exp_inst.global_position = global_position
+		exp_inst.owner_entity = owner_entity
+		get_parent().add_child(exp_inst)
