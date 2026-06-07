778b64619502a8c0ab911374034735d5d4c6f382
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-07 19:11:47 +0700

    Cập nhập script hud và scene hud: - Thêm vào biến export mana_bar đê tham chiếu tới node ManaBar. - Thêm vào cú pháp khởi tạo giá trị mana ban đầu cho mana_bar. - Thêm vào hàm _on_mana_changed để cập nhập giá trị của thanh mana.

--- scripts/system/hud/hud.gd
+++ scripts/system/hud/hud.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var hp_bar: TextureProgressBar # Biến để lưu trữ tham chiếu đến TextureProgressBar hiển thị HP của người chơi
+@export var mana_bar: TextureProgressBar
 
 var player_stats: CharacterStats = null # Biến để lưu trữ tham chiếu đến CharacterStats của người chơi
 
@@ -10,13 +11,26 @@ func _ready() -> void:
 	player_stats = GameManager.player.stats if GameManager.player else null
 	
 	if player_stats:
-		hp_bar.max_value = player_stats.max_health # Đặt giá trị tối đa của thanh HP dựa trên max_health của người chơi
-		hp_bar.value = player_stats.current_health # Đặt giá trị hiện tại của thanh HP dựa trên current_health của người chơi
+
+		# Khởi tạo giá trị máu ban đầu cho hp_bar
+		hp_bar.max_value = player_stats.max_health
+		hp_bar.value = player_stats.current_health
 		
+		# Khởi tạo giá trị mana ban đầu cho mana_bar
+		mana_bar.max_value = player_stats.max_mana
+		mana_bar.value = player_stats.current_mana
+
 	if player_stats:
 		# Kết nối tín hiệu changed của CharacterStats để cập nhật thanh HP khi máu thay đổi
 		player_stats.changed.connect(_on_health_changed)
+		player_stats.changed.connect(_on_mana_changed)
+
 
 func _on_health_changed() -> void:
 	if player_stats:
 		hp_bar.value = player_stats.current_health # Cập nhật giá trị của thanh HP khi máu của người chơi thay đổi
+
+
+func _on_mana_changed() -> void:
+	if player_stats:
+		mana_bar.value = player_stats.current_mana # Cập nhật giá trị của thanh mana
