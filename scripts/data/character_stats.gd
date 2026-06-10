46a0a1c3c1fc3bf207416dad916f7bc1e60edb1e
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-10 18:40:30 +0700

    Thêm vào các thông số stamina trong script character_stats.

--- scripts/data/character_stats.gd
+++ scripts/data/character_stats.gd
@@ -4,6 +4,7 @@ class_name CharacterStats
 
 @export var max_health: float = 100.0
 @export var max_mana: float = 100.0
+@export var max_stamina: float = 100.0
 @export var current_health: float = 100.0 :
 	set(value):
 		current_health = clamp(value, 0, max_health)
@@ -12,6 +13,11 @@ class_name CharacterStats
 @export var current_mana: float = 100.0 :
 	set(value):
 		current_mana = clamp(value, 0, max_mana)
-		emit_changed() # Thông báo khi máu thay đổi (để UI cập nhật)
+		emit_changed() # Thông báo khi mana thay đổi (để UI cập nhật)
+
+@export var current_stamina: float = 100.0 :
+	set(value):
+		current_stamina = clamp(value, 0, max_stamina)
+		emit_changed() # Thông báo khi stamina thay đổi (để UI cập nhật)
 
 @export var speed: float = 80.0
