9f961ad47047fbd5f9a511662bc586198f48ba92
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-17 19:30:48 +0700

    Cập nhập scene merchant và script merchant, Thêm vào logic xử lý chỉ định những vật phẩm và merchant sẽ bán và mở giao diện shop khi player tương tác với merchant.

--- scripts/npcs/merchant.gd
+++ scripts/npcs/merchant.gd
@@ -1,4 +1,10 @@
 extends CharacterBody2D
 
+@export var shop_goods: Array[ItemData] = [] # Danh sách vật phẩm trong shop
+
 func _on_interaction_area_on_interact() -> void:
-	print("Mở cửa hàng của Merchant!")
+	var shop_ui = GameManager.shop_ui # Tham chiếu đến giao diện shop
+	var player = GameManager.player # Tham chiếu đến player
+
+	if shop_ui and player:
+		shop_ui.open_shop(shop_goods, player) # Mở shop để bắt đầu mua vật phẩm
