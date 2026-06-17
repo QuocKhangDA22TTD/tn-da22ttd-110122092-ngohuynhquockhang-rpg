ed9de3a51c1ff2265fdf1f208f51f92a0e1ccfef
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-17 19:21:05 +0700

    Cập nhập scene game và script game, thêm vào instance của scene shop_ui  vào node UI trong scene game và tạo biến export tên shop_ui để gán instance của scene shop_ui vào biến export shop_ui của autoload GameManager để có thể tham chiếu tới giao diện shop_ui sau này.

--- scripts/core/game.gd
+++ scripts/core/game.gd
@@ -3,9 +3,11 @@ extends Node2D
 @export var enemies_container: Node2D # Tham chiếu đến container chứa các enemy
 @export var input_handler: InputHandler # Tham chiếu đến input handler để xử lý input
 @export var hotbar_ui: Control # Tham chiếu đến hotbar UI
+@export var shop_ui: Control # Tham chiếu đến hotbar UI
 
 # Hàm sẽ được gọi 1 lần khi scene đã load xong tất cả node.
 func _enter_tree() -> void:
 	GameManager.enemies_container = enemies_container # gán container chứa enemy cho biến enemies_container của GameManager
 	GameManager.input_handler = input_handler # gán input handler cho biến input_handler của GameManager
 	GameManager.hotbar = hotbar_ui # gán hotbar UI cho biến hotbar của GameManager
+	GameManager.shop_ui = shop_ui # gán shop UI cho biến shop_ui của GameManager
