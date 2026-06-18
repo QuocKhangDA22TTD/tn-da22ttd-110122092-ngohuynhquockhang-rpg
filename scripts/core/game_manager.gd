ce01476cf6536395924ba5134043be3647c653fb
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-18 23:04:26 +0700

    Cập nhập autoload game_manager, tạo thêm 2 biến là maps_container và entities_container.

--- scripts/core/game_manager.gd
+++ scripts/core/game_manager.gd
@@ -5,6 +5,8 @@ var enemies_container # Tham chiếu đến container chứa các enemy
 var input_handler # Tham chiếu đến input handler để xử lý input
 var hotbar # Tham chiếu đến hotbar UI
 var shop_ui # Tham chiếu đến shop UI
+var maps_container # Tham chiếu đến container chứa map
+var entities_container # Tham chiếu đến container chứa các entity
 
 func _ready():
 	pass
